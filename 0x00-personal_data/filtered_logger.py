b9503a462eb97a20d6d3f7b791ad679337bf6bbf
irafajs <irafajs@gmail.com>
2024-04-11 21:07:29 +0200

    TASK00 UPDATE1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,9 +5,11 @@ Shebang to create a PY script
 
 
 import re
+from typing import List as lt
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+        fields: lt[str], redaction: str, message: str, separator: str) -> str:
     """method to filter words and replace them"""
     for field in fields:
         pattern = re.escape(field) + r"=([^" + re.escape(separator) + r"]+)"
