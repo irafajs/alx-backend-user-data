e98e47d3ca24e9af4ac354be9adab9a1c265dc21
irafajs <irafajs@gmail.com>
2024-04-11 23:33:13 +0200

    TASK01

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ Shebang to create a PY script
 """
 
 
+import logging
 import re
 from typing import List as lt
 
@@ -13,4 +14,23 @@ def filter_datum(
     """method to filter words and replace them"""
     for field in fields:
         pattern = re.escape(field) + r"=([^" + re.escape(separator) + r"]+)"
-    return re.sub(pattern, field + "=" + redaction, message)
+        message = re.sub(pattern, field + "=" + redaction, message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: lt[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(
+                self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)
