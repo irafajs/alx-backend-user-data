baf42562c29426e7a2f58d377a05b348900f3fe8
irafajs <irafajs@gmail.com>
2024-04-16 17:38:03 +0200

    TASK03

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,15 @@ class Auth:
     """class to manage APU authentications"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """public method require_auth"""
-        return False
+        if path is None:
+            return True
+        elif excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        else:
+            for excluded_path in excluded_paths:
+                if path.startswith(excluded_path.rstrip('/')):
+                    return False
+            return True
 
     def authorization_header(self, request=None) -> str:
         """method to handle flask request object"""
