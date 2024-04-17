e472d11bb4fbfee085015cc0407e1a4517f436ce
irafajs <irafajs@gmail.com>
2024-04-17 12:16:59 +0200

    TASK101

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,7 +18,8 @@ class Auth:
             return True
         else:
             for excluded_path in excluded_paths:
-                if excluded_path.endswith('*') and path.startswith(excluded_path[:-1]):
+                if excluded_path.endswith(
+                        '*') and path.startswith(excluded_path[:-1]):
                     return False
                 elif path == excluded_path:
                     return False
