fa385a93c01a37b42dc8d3ae3fa9f6447105c372
irafajs <irafajs@gmail.com>
2024-04-18 22:41:47 +0200

    TASK01 UPDATE1

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -18,10 +18,10 @@ auth_type = getenv('AUTH_TYPE', 'auth')
 if auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-if auth_type == 'basic_auth':
+elif auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-if auth_type == 'session_auth':
+elif auth_type == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 
