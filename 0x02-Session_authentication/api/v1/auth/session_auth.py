10110d9b9418a2ab570a66817fe12f51a935c2c9
irafajs <irafajs@gmail.com>
2024-04-21 19:55:30 +0200

    TASK07

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,12 +4,9 @@ Shebang to create a PY script
 """
 
 from flask import jsonify, request
-from api.v1.views import app_views
 from api.v1.auth.auth import Auth
-from os import getenv
+from api.v1.views import app_views
 import uuid
-
-SESSION_NAME = getenv("SESSION_NAME", "_my_session_id")
 
 
 class SessionAuth(Auth):
@@ -40,26 +37,3 @@ class SessionAuth(Auth):
             if user_id:
                 return User.get(user_id)
         return None
-
-
-@app_views.route(
-        '/auth_session/login/', methods=['POST'], strict_slashes=False)
-def session_login() -> str:
-    """method to handle login and the sessions"""
-    email = request.form.get('email')
-    password = request.form.get('password')
-    if not email:
-        return jsonify({"error": "email missing"}), 400
-    if not password:
-        return jsonify({"error": "password missing"}), 400
-    from models.user import User
-    user = User.search({'email': email})
-    if not user:
-        return jsonify({"error": "no user found for this email"}), 404
-    if not user[0].is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
-    auth = SessionAuth()
-    session_id = auth.create_session(user[0].id)
-    response = jsonify(user[0].to_json())
-    response.set_cookie(SESSION_NAME, session_id)
-    return response
---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env pythin3
+"""
+Shebang to create a py script
+"""
+
+
+from flask import jsonify, request
+from api.v1.views import app_views
+from os import getenv
+
+
+SESSION_NAME = getenv("SESSION_NAME", "_my_session_id")
+
+
+@app_views.route(
+        '/auth_session/login/', methods=['POST'], strict_slashes=False)
+def session_login() -> str:
+    """method to handle login and the sessions"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+    from models.user import User
+    user = User.search({'email': email})
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+    if not user[0].is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+    session_id = auth.create_session(user[0].id)
+    response = jsonify(user[0].to_json())
+    response.set_cookie(SESSION_NAME, session_id)
+    return response
