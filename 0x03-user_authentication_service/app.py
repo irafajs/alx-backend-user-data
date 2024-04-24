6171f7d9f495f9a583e8470a0df20a2a15d7e952
irafajs <irafajs@gmail.com>
2024-04-24 17:31:07 +0200

    TASK11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,7 +5,7 @@ Shebang to create a py script
 
 
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 
 AUTH = Auth()
 app = Flask(__name__)
@@ -27,6 +27,21 @@ def register_users() -> str:
         return {"email": email, "message": "user created"}, 200
     except ValueError:
         return {"message": "email already registered"}, 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """method to handle user login"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    check_valid = AUTH.valid_login(email=email, password=password)
+    if check_valid:
+        session_id = AUTH.create_session(email=email)
+        response = make_response({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
